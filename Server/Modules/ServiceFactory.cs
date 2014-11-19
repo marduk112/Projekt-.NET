191fda0e7db731821c6bdcd85cc9ea665b790010
azranel <chunkyo@gmail.com>
2014-11-19 21:30:24 +0100

    #1 RegistrationService and ServiceFactory
    Topshelf added as RegistrationService. All services should be created
    through ServiceFactory.

---
+++ Server/Modules/ServiceFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Server.Modules
+{
+    class ServiceFactory
+    {
+        public static Host createRegistrationService()
+        {
+            var host = HostFactory.New(x =>
+            {
+                x.Service<RegistrationService>(s =>
+                {
+                    s.ConstructUsing(name => new RegistrationService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalService();
+                x.SetDescription("Service for handling registrations");
+                x.SetDisplayName("RegistrationService");
+                x.SetServiceName("RegistrationService");
+            });
+            return host;
+        }
+    }
+}
