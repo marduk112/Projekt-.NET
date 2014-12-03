9f46381b31e60c7c03acd1d27f9e256205d13ea9
Dawid Jaskot <dawid.jaskot@gmail.com>
2014-12-03 01:00:21 +0100

    Corrected path of Users.xml file in Const.cs

--- Common/Const.cs
+++ Common/Const.cs
@@ -3,7 +3,7 @@
     public static class Const
     {
         public const string HostName = "127.0.0.1";
-        public const string FileNameToRegAndLogin = @"Databases\Users.xml";
+        public const string FileNameToRegAndLogin = @"Users.xml";
         public static string UserLogin { get; set; }
     }
 }
