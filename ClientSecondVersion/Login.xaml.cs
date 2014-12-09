60e93148bb8b121c0af877d7972a2b9dc757771d
boolianka <boolianka@onet.eu>
2014-12-09 15:25:53 +0100

    NewDesignOfClient

---
+++ ClientSecondVersion/Login.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Client
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetLanguageDictionary();
+        }
+
+        private void SetLanguageDictionary()
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+            switch (Thread.CurrentThread.CurrentCulture.ToString())
+            {
+                case "pl":
+                    dict.Source = new Uri("..\\Resources\\Resources.xaml", UriKind.Relative);
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                    break;
+                case "en":
+                    dict.Source = new Uri("..\\Resources\\Resources.EN.xaml", UriKind.Relative);
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                    break;
+                default:
+                    dict.Source = new Uri("..\\Resources\\Resources.xaml", UriKind.Relative);
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dict);
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Ellipse_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void LblName_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnRegistry_Click(object sender, RoutedEventArgs e)
+        {
+            (new Registration()).Show();
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            (new Contacts()).Show();
+            this.Close();
+        }
+
+        private void btnPolish_Click(object sender, RoutedEventArgs e)
+        {
+            System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("pl");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            SetLanguageDictionary();
+        }
+
+        private void btnEnglish_Click(object sender, RoutedEventArgs e)
+        {
+            System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("en");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            SetLanguageDictionary();
+        }
+
+    }
+
+}
