186e4ee2070a833e4ff2f5ea0332e9e02b2bdea4
Dawid Jaskot <dawid.jaskot@gmail.com>
2015-01-16 20:47:43 +0100

    Add SQLite with linq

---
+++ Server/DataModels/User.cs
@@ -0,0 +1,51 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DataModels
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(32), NotNull, Unique]
+        public string Login { get; set; }
+        [MaxLength(32), NotNull]
+        public string Password { get; set; }
+    }
+
+    public class Database : SQLiteConnection
+    {
+        public Database(string path)
+            : base(path)
+        {
+            CreateTable<User>();
+        }
+        public User QueryUser(string userLogin)
+        {
+            return (from u in Table<User>()
+                    where u.Login == userLogin
+                    select u).FirstOrDefault();
+        }
+        public IEnumerable<User> QueryAllUsers()
+        {
+            return from u in Table<User>()
+                   orderby u.Login
+                   select u;
+        }
+
+        public void RegisterUser(string userLogin, string userPassword)
+        {
+            var user = QueryUser(userLogin);
+            if (user == null)
+            {
+                user = new User {Login = userLogin, Password = userPassword};
+                Insert(user);
+            }
+        }
+    }
+
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Server.Modules;
 using Topshelf;
+using SQLite;
 
 namespace Server
 {
