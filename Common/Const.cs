d1f4167a88a5caa803a63eb6d1204f14afdd5971
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-01-19 02:01:25 +0100

    change IP address of RabbitMQ server

--- Common/Const.cs
+++ Common/Const.cs
@@ -2,7 +2,7 @@
 {
     public static class Const
     {
-        public const string HostName = "192.168.1.254";
+        public const string HostName = "127.0.0.1";
         /// <summary>
         /// Was used to login and register users. Deprecated.
         /// </summary>
